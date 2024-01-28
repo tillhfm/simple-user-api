bfe7c65f771c3a59f03b793bb161d49e2c585f8b
trxsson <mail@trxsson.de>
2024-01-28 02:23:29 +0100

    Added documentation to UserApiApplication class

--- src/main/java/de/trxsson/userapi/UserApiApplication.java
+++ src/main/java/de/trxsson/userapi/UserApiApplication.java
@@ -3,9 +3,17 @@ package de.trxsson.userapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * UserApiApplication is the main class that starts the User API application.
+ */
 @SpringBootApplication
 public class UserApiApplication {
 
+	/**
+	 * The main method of the UserApiApplication class.
+	 *
+	 * @param args the command line arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(UserApiApplication.class, args);
 	}
