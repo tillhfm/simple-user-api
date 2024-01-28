07984b83ec9abddfde8b86b3a2053b4ea1078d82
trxsson <mail@trxsson.de>
2024-01-28 04:22:35 +0100

    Remove unused imports from User entity class

--- src/main/java/de/trxsson/userapi/entity/User.java
+++ src/main/java/de/trxsson/userapi/entity/User.java
@@ -1,8 +1,5 @@
 package de.trxsson.userapi.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
